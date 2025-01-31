b39b644fc75189d66f918465ca44aaea654b3ecc
ABOGO <abogonono1@gmail.com>
2025-01-31 20:20:48 +0100

    dbconnection documented

--- src/configs/dbConnect.js
+++ src/configs/dbConnect.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
+/**
+ * Asynchronously connects to the MongoDB database using the connection URI
+ * specified in the environment variable `MONGODB_URI`.
+ * 
+ * @async
+ * @function connectDB
+ * @returns {Promise<void>} Resolves when the connection is successfully established.
+ * @throws Will throw an error if the connection fails, and exits the process with status code 1.
+ */
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
